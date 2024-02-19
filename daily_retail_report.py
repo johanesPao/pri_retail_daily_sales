10c7f813e454f0e0b682e5dac74c1bdd57e78e00
Johanes Gregorius Indra Pradana Pao <johanes.pao@pri.co.id>
2024-02-19 18:06:04 +0700

    Pengerjaan df generation function pada fungsi.py

--- daily_retail_report.py
+++ daily_retail_report.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import os
 import argparse
 from datetime import date, timedelta
-import xlwings as xw
+from typing import Literal
 
 from utils.fungsi import generate_df_utama, generate_df_single_sbu, generate_report
 
@@ -24,10 +24,35 @@ tgl_laporan = parser.parse_args().tanggal
 # generate dataframe utama
 df_utama = generate_df_utama()
 
+# konfigurasi
+konfig = [
+    {"nama_tabel": "sbu", "nama_sheet": "Dashboard SBU"},
+    {"nama_tabel": "area", "nama_sheet": "Dashboard Area"},
+    {"nama_tabel": "cnc", "nama_sheet": "Dashboard Comp & Non Comp Store"},
+    {"nama_tabel": "odd", "nama_sheet": "Our Daily Dose"},
+    {"nama_tabel": "fisik", "nama_sheet": "Fisik"},
+    {"nama_tabel": "bazaar", "nama_sheet": "Bazaar"},
+    {"nama_tabel": "detailed", "nama_sheet": "Detailed Daily Sales & Est."},
+]
+
+# setup worksheet list
+worksheet_list = [
+    "Dashboard SBU",
+    "Dashboard Area",
+    "Dashboard Comp & Non Comp Store",
+    "Our Daily Dose",
+    "Fisik",
+    "Bazaar",
+    "Detailed Daily Sales & Est.",
+]
+
+# generate tabel untuk dimuat pada masing - masing sheet
+
+
 # path report template dan direktori_output
 report_dirname = "reports"
 dir_ini = Path(__file__).resolve().parent
 dir_report = Path(os.path.join(dir_ini, report_dirname)).resolve()
 
 # generate report
-generate_report(dir_ini, dir_report, df_utama, tgl_laporan)
+generate_report(dir_ini, dir_report, konfig, df_utama, tgl_laporan)
--- utils/fungsi.py
+++ utils/fungsi.py
@@ -1,14 +1,16 @@
 import os
 import math
 from typing import Literal
+from numpy import isnan
 import pandas as pd
 import xlwings as xw
+import xlsxwriter as xlsx
 from O365 import Account, FileSystemTokenBackend
 from dotenv import load_dotenv
 from datetime import date, timedelta
 from pathlib import Path
 
-from utils.kueri import kueri_sales, kueri_target
+from utils.kueri import kueri_area_toko, kueri_sales, kueri_target
 from utils.pg import PostgreSQL
 
 file_konfig = ".config.env"
@@ -86,22 +88,22 @@ def generate_df_utama(
     # join dataframe
     df_merge = df_target.merge(df_sales, left_on="Toko", right_on="Toko")
     # Target Achievement
-    df_merge["Daily Ach."] = df_merge.apply(
-        lambda baris: persen_ach(baris["Daily Sales"], baris["Daily Target"]), axis=1
-    )
-    df_merge["WTD Ach."] = df_merge.apply(
-        lambda baris: persen_ach(baris["WTD Sales"], baris["WTD Target"]), axis=1
-    )
-    df_merge["MTD Ach."] = df_merge.apply(
-        lambda baris: persen_ach(baris["MTD Sales"], baris["MTD Target"]), axis=1
-    )
-    # LY Achievement
-    df_merge["WTD LY Ach."] = df_merge.apply(
-        lambda baris: persen_ach(baris["WTD Sales"], baris["WTD LY Sales"]), axis=1
-    )
-    df_merge["MTD LY Ach."] = df_merge.apply(
-        lambda baris: persen_ach(baris["MTD Sales"], baris["MTD LY Sales"]), axis=1
-    )
+    # df_merge["Daily Ach."] = df_merge.apply(
+    #     lambda baris: persen_ach(baris["Daily Sales"], baris["Daily Target"]), axis=1
+    # )
+    # df_merge["WTD Ach."] = df_merge.apply(
+    #     lambda baris: persen_ach(baris["WTD Sales"], baris["WTD Target"]), axis=1
+    # )
+    # df_merge["MTD Ach."] = df_merge.apply(
+    #     lambda baris: persen_ach(baris["MTD Sales"], baris["MTD Target"]), axis=1
+    # )
+    # # LY Achievement
+    # df_merge["WTD LY Ach."] = df_merge.apply(
+    #     lambda baris: persen_ach(baris["WTD Sales"], baris["WTD LY Sales"]), axis=1
+    # )
+    # df_merge["MTD LY Ach."] = df_merge.apply(
+    #     lambda baris: persen_ach(baris["MTD Sales"], baris["MTD LY Sales"]), axis=1
+    # )
     # reorder dataframe
     df_merge = df_merge[
         [
@@ -109,75 +111,194 @@ def generate_df_utama(
             "Nama Toko",
             "Daily Sales",
             "Daily Target",
-            "Daily Ach.",
+            # "Daily Ach.",
             "WTD Sales",
             "WTD Target",
-            "WTD Ach.",
+            # "WTD Ach.",
             "WTD LY Sales",
-            "WTD LY Ach.",
+            # "WTD LY Ach.",
             "MTD Sales",
             "MTD Target",
-            "MTD Ach.",
+            # "MTD Ach.",
             "MTD LY Sales",
-            "MTD LY Ach.",
+            # "MTD LY Ach.",
         ]
     ]
-    # return df_merge seabgai dataframe utama
+    # return df_merge sebagai dataframe utama
     return df_merge
 
+def generate_df_sbu(
+        data: pd.DataFrame
+) -> pd.DataFrame:
+    # create SBU categorization column
+    data['SBU'] = data["Toko"].apply(
+        lambda toko: 'Our Daily Dose' if toko.startswith("OD") else 
+        'Fisik' if (
+            toko.startswith("FS") | 
+            toko.startswith("FF") | 
+            toko.startswith("FO")
+        ) else 'Bazaar')
+    # drop kolom Toko dabn Nama Toko
+    df_sbu = data.drop(['Toko', 'Nama Toko'], axis=1)
+    # pandas groupby SBU
+    df_sbu = df_sbu.groupby(['SBU'], as_index=False).sum()
+    # reset index menjadi 1
+    df_sbu.index = range(1, len(df_sbu) + 1)
+    # append stores total
+    df_sbu.loc['STORES TOTAL'] = df_sbu.sum(numeric_only=True, axis=0)
+    # return dataframe sbu
+    return df_sbu
+
+def generate_df_area(
+        data: pd.DataFrame,
+        tgl_laporan: date = date.today() - timedelta(days=1),
+) -> pd.DataFrame:
+    print(data)
+    # file konfigurasi environment
+    file_konfig = ".config.env"
+    direktori = Path(os.path.dirname(__file__)).parent
+    file_path = os.path.join(direktori, file_konfig)
+    # load konf database pada .config.env
+    load_dotenv(file_path)
+    # inisiasi kelas PostgreSQL
+    sql = PostgreSQL(
+        host=PG_HOST,
+        port=PG_PORT,
+        dbname=PG_DB,
+        username=PG_USER,
+        pwd=PG_PWD,
+    )
+    # konstruksi dataframe
+    # area_toko
+    df_area = sql.lakukan_kueri(kueri_area_toko(tgl_laporan))
+    # join dataframe df_area dan df_utama
+    df_merge = data.merge(df_area, left_on="Toko", right_on="Toko")
+    print(df_merge)
+    # drop kolom Toko dan Nama Toko
+    df_area = df_merge.drop(['Toko', 'Nama Toko'], axis=1)
+    # pandas groupby Area
+    df_area = df_area.groupby(['Area'], as_index=False).sum()
+    # reset index menjadi 1
+    df_area.index = range(1, len(df_area) + 1)
+    # append stores total
+    df_area.loc['STORES TOTAL'] = df_area.sum(numeric_only=True, axis=0)
+    # return df_area
+    return df_area
+
+def generate_df_cnc(
+        data: pd.DataFrame
+) -> pd.DataFrame:
+    print(data)
+    # fungsi kategorisasi cnc
+    def kategorisasi_cnc(kode_toko: str, mtd_ly_sales: int) -> str:
+        match kode_toko[:2]:
+            case "OD":
+                return "Comp Stores ODD" if not(isnan(mtd_ly_sales)) else "Non Comp Stores ODD"
+            case "FS" | "FF" | "FO":
+                return "Comp Stores Fisik" if not(isnan(mtd_ly_sales)) else "Non Comp Stores Fisik"
+            case "BZ":
+                return "Bazaar Stores"
+            case _:
+                return "Comp Stores Others" if not(isnan(mtd_ly_sales)) else "Non Comp Store Others"
+            
+    # kategorisasi comp-non comp berdasar ketersediaan data MTD LY Sales
+    data['CNC'] = data.apply(
+        lambda baris: kategorisasi_cnc(baris['Toko'], baris['MTD LY Sales']),
+        axis=1
+        )
+    # drop kolom Toko dan Nama Toko
+    df_cnc = data.drop(["Toko", "Nama Toko"], axis=1)
+    # groupby CNC
+    df_cnc = df_cnc.groupby(['CNC'], as_index=False).sum()
+    # reset index dari 1
+    df_cnc.index = range(1, len(df_cnc) + 1)
+    # append stores total
+    df_cnc.loc['STORES TOTAL'] = df_cnc.sum(numeric_only=True, axis=0)
+    # return df_cnc
+    return df_cnc
+
 def generate_nama_sbu(
-    sbu: Literal["ODD", "Fisik", "Bazaar"],
+    sbu: Literal["odd", "fisik", "bazaar"],
     tgl: date
 ) -> str:
     match sbu:
-        case "ODD":
+        case "odd":
             nama_sbu = "Our Daily Dose".upper()
-        case "Fisik":
+        case "fisik":
             nama_sbu = "Fisik".upper()
-        case _:
+        case "bazaar":
             nama_sbu = "Bazaar".upper()
+        case _:
+            nama_sbu = "Others".upper()
     return f"{nama_sbu} NATIONAL SALES {tgl.strftime("%d %B %Y").upper()}"
 
 def generate_df_single_sbu(
     data: pd.DataFrame, 
-    sbu: Literal["ODD", "Fisik", "Bazaar"]
+    sbu: Literal["odd", "fisik", "bazaar"]
 ) -> pd.DataFrame:
     match sbu:
-        case "ODD":
-            df_sbu = data[data["Toko"].str.startswith("OD")]
-        case "Fisik":
-            df_sbu = data[
-                (data["Toko"].str.startswith("FS")) | 
-                (data["Toko"].str.startswith("FF")) | 
-                (data["Toko"].str.startswith("FO"))
-            ]
+        case "odd":
+            # sbu main dataframe
+            df_sbu = data[data["Toko"].str.startswith("OD")].copy()
+            # sbu reset index to 1
+            df_sbu.index = range(1, len(df_sbu) + 1)
+            # append Comp Stores Total & Stores Total
+            df_sbu.loc['COMP STORES TOTAL'] = df_sbu.query('`MTD LY Sales`.notna()').sum(numeric_only=True, axis=0)
+            df_sbu.loc['STORES TOTAL'] = df_sbu.query('Toko.notna()').sum(numeric_only=True, axis=0)
+        case "fisik":
+            # buat dataframe per sub sbu
+            df_fs = data.copy().loc[data["Toko"].str.startswith("FS")]
+            df_ff = data.copy().loc[data["Toko"].str.startswith("FF")]
+            df_fo = data.copy().loc[data["Toko"].str.startswith("FO")]
+            # buat subtotal jika row df lebih besar dari 0
+            if df_fs.shape[0] > 0:
+                df_fs.index = range(1, len(df_fs) + 1)
+                df_fs.loc['FISIK SPORT TOTAL'] = df_fs.sum(numeric_only=True, axis=0)
+            if df_ff.shape[0] > 0:
+                df_ff.index = range(1, len(df_ff) + 1)
+                df_ff.loc['FISIK FOOTBALL TOTAL'] = df_ff.sum(numeric_only=True, axis=0)
+            if df_fo.shape[0] > 0:
+                df_fo.index = range(1, len(df_fo) + 1)
+                df_fo.loc['FACTORY OUTLET TOTAL'] = df_fo.sum(numeric_only=True, axis=0)
+            # concat ketiga df sub sbu
+            df_sbu = pd.concat([df_fs, df_ff, df_fo], ignore_index=False)
+            # append Comp Stores Total & Stores Total
+            df_sbu.loc['COMP STORES TOTAL'] = df_sbu.query('`MTD LY Sales`.notna() and Toko.notna()').sum(numeric_only=True, axis=0)
+            df_sbu.loc['STORES TOTAL'] = df_sbu.query('Toko.notna()').sum(numeric_only=True, axis=0)
+            # df_sbu = data[
+            #     (data["Toko"].str.startswith("FS")) | 
+            #     (data["Toko"].str.startswith("FF")) | 
+            #     (data["Toko"].str.startswith("FO"))
+            # ]
         case _:
             df_sbu = data[data["Toko"].str.startswith("BZ")]
-    df_sbu.index = range(1, len(df_sbu) + 1)
-    print(df_sbu)
+    # df_sbu.index = range(1, len(df_sbu) + 1)
+    # print(df_sbu)
     return df_sbu
 
-def generate_report(
-    path_template: Path, 
+def generate_report( 
+    path_template: Path,
     path_output: Path, 
+    konfigurasi: list[dict[str,str]],
     dataframe: pd.DataFrame, 
     tgl: date
 ) -> None:
-    data = dict(
-        title="PRI Daily Retail Report",
-        df_utama=dataframe.reset_index(),
-        judul_odd=generate_nama_sbu("ODD", tgl),
-        df_odd=generate_df_single_sbu(dataframe, "ODD").reset_index(),
-        judul_fisik=generate_nama_sbu("Fisik", tgl),
-        df_fisik=generate_df_single_sbu(dataframe, "Fisik").reset_index(),
-        judul_bazaar=generate_nama_sbu("Bazaar", tgl),
-        df_bazaar=generate_df_single_sbu(dataframe, "Bazaar").reset_index() if 
-            generate_df_single_sbu(dataframe, "Bazaar").shape[0] > 0 else
-            ["Data tidak ditemukan"],
-    )
-    with xw.App(visible=False) as app:
-        book = app.render_template(
-            path_template / "report_template.xlsx",
-            path_output / f"PRI_Retail_Daily_Sales_Report_{tgl.strftime("%d_%b_%Y")}.xlsx",
-            **data,
-        )
+    print(generate_df_single_sbu(dataframe, "odd"))
+    # data = dict(
+    #     title="PRI Daily Retail Report",
+    #     df_utama=dataframe.reset_index(),
+    #     judul_odd=generate_nama_sbu("odd", tgl),
+    #     df_odd=generate_df_single_sbu(dataframe, "odd").reset_index(),
+    #     judul_fisik=generate_nama_sbu("fisik", tgl),
+    #     df_fisik=generate_df_single_sbu(dataframe, "fisik").reset_index(),
+    #     judul_bazaar=generate_nama_sbu("bazaar", tgl),
+    #     df_bazaar=generate_df_single_sbu(dataframe, "bazaar").reset_index() if 
+    #         generate_df_single_sbu(dataframe, "bazaar").shape[0] > 0 else
+    #         ["Data tidak ditemukan"],
+    # )
+    # with xw.App(visible=False) as app:
+    #     book = app.render_template(
+    #         path_template / "report_template.xlsx",
+    #         path_output / f"PRI_Retail_Daily_Sales_Report_{tgl.strftime("%d_%b_%Y")}.xlsx",
+    #         **data,
+    #     )
