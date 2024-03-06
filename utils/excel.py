20af1cbe0cab997fae02319dfb703dfca1686447
Johanes Gregorius Indra Pradana Pao <johanes.pao@pri.co.id>
2024-03-06 09:42:26 +0700

    Penyelesaian penulisan data rdsr pada file excel

--- utils/excel.py
+++ utils/excel.py
@@ -190,19 +190,137 @@ def buat_header_rdsr(
     objek_sheet.write(baris + 2, offset_kolom + 11, fh_ly_sales_subjudul, format_subjudul_fh_mid)
     objek_sheet.write(baris + 2, offset_kolom + 12, fh_ly_persen_subjudul, format_subjudul_fh_last)
 
+def set_kolom_field_rdsr(
+    objek_sheet: xlsx.Workbook.worksheet_class,
+    jenis_df_nt: bool        
+) -> None:
+    if jenis_df_nt:
+        objek_sheet.set_column(0,0,29)
+    else:
+        objek_sheet.set_column(0,0, 4.57)
+        objek_sheet.set_column(1,1, 13.14)
+        objek_sheet.set_column(2,2, 51)
+
+def set_kolom_data_rdsr(
+    objek_sheet: xlsx.Workbook.worksheet_class,
+    kolom_awal: int,
+    jumlah_kolom: int = 13,
+    lebar_kolom: float = 15.57
+) -> None:
+    # set lebar kolom
+    objek_sheet.set_column(kolom_awal, jumlah_kolom + kolom_awal - 1, lebar_kolom)
+    # set freeze pane
+    objek_sheet.freeze_panes(3, kolom_awal)
+
+def buat_list_kolom_persen(
+    jumlah_kolom_field: int = 1,
+) -> list[int]:
+    return [jumlah_kolom_field + 2, jumlah_kolom_field + 5, jumlah_kolom_field + 7, jumlah_kolom_field + 10, jumlah_kolom_field + 12]
+
+
 def loop_dataframe_rdsr(
     kunci_df: Literal["sbu", "area", "cnc", "odd", "fisik", "bazaar"],
     jenis_df_nt: bool,
+    objek_buku: xlsx.Workbook,
+    objek_sheet: xlsx.Workbook.worksheet_class,
     df: pd.DataFrame,
 ) -> None:
-    print(f"Melakukan iterasi data pada DataFrame {kunci_df}...")
+    print(f"Setup styling {kunci_df}...")
+    # judul_cnc
+    judul_cnc = ["COMP STORES TOTAL", "NON COMP STORES TOTAL", "STORES TOTAL"]
+    # format_judul_field
+    format_judul_field = objek_buku.add_format({
+        "bold": 1,
+        "fg_color": "#E2EFDA"
+    })
+    format_judul_cnc = objek_buku.add_format({
+        "bold": 1,
+        "align": "center",
+        "fg_color": "#FFF2CC"
+    })
+    print(f"Melakukan iterasi data {kunci_df}...")
+    # set baris awal
+    baris = 3
+    # format persen
+    format_persen = objek_buku.add_format({
+        "bold": 1,
+        "num_format": "0.0%",
+        "fg_color": "#E2EFDA"
+    })
+    format_persen_cnc = objek_buku.add_format({
+        "bold": 1,
+        "num_format": "0.0%",
+        "fg_color": "#FFF2CC"
+    })
+    # format nominal
+    format_nominal = objek_buku.add_format({
+        "num_format": "#,##0.00,,_)jt;(#,##0.00,,)jt",
+        "fg_color": "#E2EFDA"
+    })
+    format_nominal_cnc = objek_buku.add_format({
+        "num_format": "#,##0.00,,_)jt;(#,##0.00,,)jt",
+        "fg_color": "#FFF2CC"
+    })
+    # set kolom dengan num_format persen (zero-based)
+    kolom_persen = buat_list_kolom_persen(1 if jenis_df_nt else 3)
     # iterasi pada dataframe df
-    for indeks, baris in df.iterrows():
+    for indeks, baris_data in df.iterrows():
         # jika jenis_df_nt True maka kita tidak perlu untuk melakukan evaluasi indeks pada dataframe    
         if jenis_df_nt:
-            print(baris)
+            for hitung in range(len(baris_data)):
+                # pada kolom 0 kita ingin mengevaluasi apakah indeks adalah str atau tidak
+                if hitung == 0:
+                    if isinstance(indeks, str):
+                        # tulis indeks pada kolom 0
+                        objek_sheet.write(baris, hitung, indeks, format_judul_cnc if indeks in judul_cnc else format_judul_field)
+                    else:
+                        objek_sheet.write(baris, hitung, baris_data.iloc[hitung], format_judul_field)
+                # jika bukan kolom 0
+                else:
+                    if isinstance(indeks, str) and indeks in judul_cnc:
+                        objek_sheet.write(baris, hitung, baris_data.iloc[hitung], format_persen_cnc if hitung in kolom_persen else format_nominal_cnc)
+                    else:
+                        objek_sheet.write(baris, hitung, baris_data.iloc[hitung], format_persen if hitung in kolom_persen else format_nominal)
         else:
-            print(indeks, baris)
+            # dalam kasus df bukanlah jenis_df_nt, maka kita akan melakukan pengisian tiga kolom awal dengan
+            # No, Store Code dan Location
+            # kita juga akan mengoffset loop + 1 untuk mengkompensasi penulisan indeks sebagai No atau CNC
+            offset_kolom_regular = 1
+            offset_kolom_cnc = 2 # zero-based
+            for hitung in range(len(baris_data) + offset_kolom_regular):
+                # jika hitung == 0:
+                if hitung == 0:
+                    # cek jika indeks adalah str
+                    if isinstance(indeks, str):
+                        # merge range cell dengan R1C1 1,3
+                        objek_sheet.merge_range(baris, 0, baris, 2, indeks, format_judul_cnc if indeks in judul_cnc else format_judul_field)
+                    else:
+                        # tulis indeks tanpa merge
+                        objek_sheet.write(baris, hitung, indeks, format_judul_field)
+                elif hitung > 0 and hitung <= offset_kolom_cnc:
+                    # cek jika indeks bukan instance of str, jika instance of str, do nothing
+                    if not isinstance(indeks, str):
+                        objek_sheet.write(baris, hitung, baris_data.iloc[hitung - offset_kolom_regular], format_judul_field)
+                # jika hitung lebih besar atau sama dengan offset_kolom_cnc
+                else:
+                    if isinstance(indeks, str) and indeks in judul_cnc:
+                        objek_sheet.write(baris, hitung, baris_data.iloc[hitung - offset_kolom_regular], format_persen_cnc if hitung in kolom_persen else format_nominal_cnc)
+                    else:
+                        objek_sheet.write(baris, hitung, baris_data.iloc[hitung - offset_kolom_regular], format_persen if hitung in kolom_persen else format_nominal)
+        # increment baris
+        baris += 1
+        # lebar kolom dan freeze pane
+        set_kolom_field_rdsr(objek_sheet, jenis_df_nt)
+        set_kolom_data_rdsr(objek_sheet, 1 if jenis_df_nt else 3)
+        # sembunyikan baris yang tidak dipergunakan
+        objek_sheet.set_default_row(hide_unused_rows=True)
+        # kolom akhir pada excel (zero-based R1C1)
+        kolom_akhir_excel = 16383
+        # kolom_awal_sembunyi (zero-based R1C1, kondisional terhadap jenis_df_nt)
+        kolom_awal_sembunyi = len(baris_data) if jenis_df_nt else len(baris_data) + 1
+        # sembunyikan kolom yang tidak dipergunakan
+        objek_sheet.set_column(kolom_awal_sembunyi, kolom_akhir_excel, None, None, {"hidden": True})
+
 
 def tulis_data_rdsr(
     file: str,
@@ -218,15 +336,12 @@ def tulis_data_rdsr(
     # untuk list kunci semua dataframe dalam objek_df, merujuk pada fungsi.py baris 385-392
     # buat list kunci dalam objek_df
     kunci_df = list(objek_df.keys())
-    # buat list kunci masing - masing jenis dataframe
+    # buat list kunci dataframe jenis nt
     kunci_df_nt = [kunci_df[0], kunci_df[1], kunci_df[2]]
-    kunci_df_t = [kunci_df[3], kunci_df[4], kunci_df[5]]
     # buat workbook report
     buku_report = xlsx.Workbook(file)
     # loop pada objek_df dan lakukan penulisan data
     for kunci, data in objek_df.items():
-        # untuk debugging
-        print(f"{kunci}: ", data.shape)
         # jika kunci terdapat dalam nama_sheet.keys()
         if kunci in nama_sheet.keys():
             # buat sheet dengan nilai dimana kunci pada objek_df sebagai kunci pada nama_sheet
@@ -235,15 +350,12 @@ def tulis_data_rdsr(
             sheet_report.set_zoom(80)
             # sembunyikan gridline
             sheet_report.hide_gridlines(2)
-            # define tinggi dan lebar data (data.shape tidak menghitung header/nama kolom dan index pada
-            # dataframe)
-            tinggi_data, lebar_data = data.shape
             # buat header
             buat_header_rdsr(
                 tgl, buku_report, sheet_report, kunci, kunci in kunci_df_nt, data.shape[1]
             )
             # looping dalam dataframe
-            loop_dataframe_rdsr(kunci, kunci in kunci_df_nt, data)
+            loop_dataframe_rdsr(kunci, kunci in kunci_df_nt, buku_report, sheet_report, data)
 
     # tutup dan output workbook report
     buku_report.close()
